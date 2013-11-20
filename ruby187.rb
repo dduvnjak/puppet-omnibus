8ae6666c7521311f144667434f32efead7e25791
Dario Duvnjak <dario.duvnjak@gmail.com>
2013-11-20 14:25:36 +0100

    Create ruby187.rb

---
+++ ruby187.rb
@@ -0,0 +1,67 @@
+class Ruby193 < FPM::Cookery::Recipe
+  description 'The Ruby virtual machine'
+
+  name 'ruby'
+  version '1.8.7.374'
+  revision 1
+  homepage 'http://www.ruby-lang.org/'
+  source 'http://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p374.tar.bz2'
+  sha256 'b4e34703137f7bfb8761c4ea474f7438d6ccf440b3d35f39cc5e4d4e239c07e3'
+
+  maintainer '<github@tinycat.co.uk>'
+  vendor     'fpm'
+  license    'The Ruby License'
+
+  section 'interpreters'
+
+  platforms [:ubuntu, :debian] do
+    build_depends 'autoconf',
+                  'libreadline6-dev',
+                  'bison',
+                  'zlib1g-dev',
+                  'libssl-dev',
+                  'libncurses5-dev',
+                  'build-essential',
+                  'libffi-dev',
+                  'libgdbm-dev'
+    depends 'libffi6',
+            'libncurses5',
+            'libreadline6',
+            'libssl1.0.0',
+            'libtinfo5',
+            'zlib1g',
+            'libgdbm3'
+  end
+
+  platforms [:fedora, :redhat, :centos] do
+    build_depends 'rpmdevtools',
+                  'libffi-devel',
+                  'autoconf',
+                  'bison',
+                  'libxml2-devel',
+                  'libxslt-devel',
+                  'openssl-devel',
+                  'gdbm-devel'
+    depends 'zlib',
+            'libffi',
+            'gdbm'
+  end
+  platforms [:fedora] do depends.push('openssl-libs') end
+  platforms [:redhat, :centos] do depends.push('openssl') end
+
+  def build
+    configure :prefix => destdir,
+              'enable-shared' => true,
+              'disable-install-doc' => true,
+              'with-opt-dir' => destdir
+    make
+  end
+
+  def install
+    make :install
+    # Shrink package.
+    rm_f "#{destdir}/lib/libruby-static.a"
+    safesystem "strip #{destdir}/bin/ruby"
+    safesystem "find #{destdir} -name '*.so' -or -name '*.so.*' | xargs strip"
+  end
+end
