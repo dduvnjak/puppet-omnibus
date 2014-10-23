105dc5b608a56bacd8fb192c285bbb9aaa2fa60b
dduvnjak <dario.duvnjak@gmail.com>
2014-10-23 16:27:04 +0200

    Make sure bundler is installed correctly

--- puppet_provisioning/site.pp
+++ puppet_provisioning/site.pp
@@ -1,16 +1,18 @@
-node "precise" {
-  package {"ruby": ensure => installed}
-  package {"ruby-dev": ensure => installed}
-  package {"git": ensure => installed}
+node "precise", "wheezy" {
+  $package_list = ['ruby', 'rubygems', 'ruby-dev', 'git', 'build-essential', 'curl']
+  package {$package_list: ensure => installed, before => Package['bundler']}
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
+  }
+  Exec['apt-update'] -> Package <| |>
 }
 
 node "centos6" {
-  package {"ruby": ensure => installed}
-  package {"git":  ensure => installed}
+  $package_list = ['ruby', 'rubygems', 'ruby-devel', 'git']
+  package {$package_list:  ensure => installed, before => Package['bundler']}
 }
 
-  package { 'bundler':
-    ensure => installed,
-    provider => 'gem',
-    require => Package['ruby'],
-  }
+package { 'bundler':
+  ensure => installed,
+  provider => 'gem',
+}
